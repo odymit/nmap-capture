c4d7d9fb4bcdb6270707ab41b261bf439165d381
omitedy <omited@163.com>
2021-08-08 23:25:01 +0800

    fix ps log exec once

--- nmap_capture.py
+++ nmap_capture.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
-# get local area network boundary
 import os 
 import re
 from netaddr import IPNetwork
@@ -9,10 +8,13 @@ import subprocess
 import datetime
 from utils import get_ip_local, get_netmask
 
+
+# get local area network boundary
 ip = get_ip_local()
 # open tcpdump to capute flows
 netmask = get_netmask(ip)
 network = IPNetwork(str(IPNetwork(str(ip)+str('/')+str(netmask)).network)+'/'+str(netmask))
+
 def start_capture(cmd=''):
     time = datetime.datetime.now()
     capture_cmd = "tcpdump -i wlp82s0 -vnn net {ip} -w ./capture/{name}.pcap".format(
@@ -20,13 +22,19 @@ def start_capture(cmd=''):
     tcpdump = subprocess.Popen(capture_cmd, shell=True)
     os.popen("ps -ef | grep nmap > ./capture/{name}.txt".format(name=time.strftime("%Y-%m-%d_%H:%M:%S")+'_nmap_port'))
     return tcpdump
+
 dump = start_capture()
+
 # get alive ip list
 print("scanning lan alive host, this may take a few minutes...")
 cmd_out = os.popen("nmap -sP "+ str(ip) + ' ' + str(netmask))
 content = cmd_out.read()
 p = "Nmap scan report for ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\nHost is up"
 alive_ips = re.findall(p, content)
+print("found ", len(alive_ips), " hosts is up in local area network.")
+for ip in alive_ips:
+    print(ip)
+
 
 '''sS, sT, sF, sX, sN, sV, sU, sO, sA'''
 cmd_list = [
@@ -44,5 +52,8 @@ for each_ip in alive_ips:
     for nmap_cmd in cmd_list:
         cmd = nmap_cmd + str(each_ip) + ' ' + str(netmask)
         print(cmd)
-        os.system(cmd)
+        time = datetime.datetime.now()
+        ret = os.popen(cmd)
+        os.popen("ps -ef | grep nmap > ./capture/{name}.txt".format(name=time.strftime("%Y-%m-%d_%H:%M:%S")+'_nmap_port'))
+        ret.read()
 dump.terminate()
